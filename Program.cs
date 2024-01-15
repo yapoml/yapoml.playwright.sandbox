3c0d0e10b3700e713af06f4e0de6ddfb8fc8ebe3
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2024-01-15 16:07:56 +0300

    Add simple sandbox

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Playwright;
+
+var playwright = await Playwright.CreateAsync();
+var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+var driver = await browser.NewPageAsync();
+
+var ya = driver.Ya(options => options.WithBaseUrl("https://github.com"));
+
+ya.SearchPage.Open(query: "yapoml")
+    .Repositories[r => r.Name == "yapoml/yapoml"]
+    .Name.Click();
+
+ya.RepositoryPage.Star.Click();
